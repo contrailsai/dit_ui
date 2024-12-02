be1f951bd5434fddfe9a6356de02cfefacc3b218
shrey19702 <shrey19702@gmail.com>
2024-12-02 19:02:40 +0530

    add to middleware

--- src/utils/supabase/middleware.js
+++ src/utils/supabase/middleware.js
@@ -47,6 +47,12 @@ export async function updateSession(request) {
     url.pathname = '/login'
     return NextResponse.redirect(url)
   }
+  else if (user && request.nextUrl.pathname.startsWith('/login')){
+        // no user, potentially respond by redirecting the user to the login page
+        const url = request.nextUrl.clone()
+        url.pathname = '/'
+        return NextResponse.redirect(url)
+  }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
